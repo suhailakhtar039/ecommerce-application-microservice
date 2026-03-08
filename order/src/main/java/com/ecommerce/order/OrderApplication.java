63d033c057dfc96ceabb115f98648148237dedbd
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-08 16:14:56 +0530

    setting time zone Asia/Kolkata explicitly

--- order/src/main/java/com/ecommerce/order/OrderApplication.java
+++ order/src/main/java/com/ecommerce/order/OrderApplication.java
@@ -3,10 +3,13 @@ package com.ecommerce.order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class OrderApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(OrderApplication.class, args);
 	}
 
