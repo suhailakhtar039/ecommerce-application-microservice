99b0d9a2b8dede54c65930328512bb6c70eac741
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-10 22:39:34 +0530

    start auditing for createdAt and updatedAt

--- user/src/main/java/com/ecommerce/user/UserApplication.java
+++ user/src/main/java/com/ecommerce/user/UserApplication.java
@@ -2,9 +2,11 @@ package com.ecommerce.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 import java.util.TimeZone;
 
+@EnableMongoAuditing
 @SpringBootApplication
 public class UserApplication {
 
