3b8cd20eeff2f09dec1d8051947e351829c43093
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-12 22:45:29 +0530

    segregate entire service

--- user/src/main/java/com/ecommerce/user/UserApplication.java
+++ user/src/main/java/com/ecommerce/user/UserApplication.java
@@ -1,8 +1,11 @@
 package com.ecommerce.user;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 import java.util.TimeZone;
 
@@ -13,6 +16,11 @@ public class UserApplication {
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(UserApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner checkDb(MongoTemplate mongoTemplate) {
+		return args -> System.out.println("MongoDB DB NAME: " + mongoTemplate.getDb().getName());
 	}
 
 }
