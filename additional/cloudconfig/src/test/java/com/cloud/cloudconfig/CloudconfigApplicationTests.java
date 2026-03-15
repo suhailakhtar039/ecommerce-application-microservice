20c8bbac74aa65d9fc2a3180745ed2ce3b7c392f
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-15 14:04:46 +0530

    refactoring and moving 2 folders to additional

---
+++ additional/cloudconfig/src/test/java/com/cloud/cloudconfig/CloudconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ additional/config/src/main/java/com/info/config/ConfigDemoApplication.java
@@ -0,0 +1,13 @@
+package com.info.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigDemoApplication.class, args);
+	}
+
+}
