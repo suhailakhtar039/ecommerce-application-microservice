72adb6ae3dda69dcc332bf9475a185535de9ec91
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-19 23:06:10 +0530

    InstanceController created in provide

---
+++ provider/src/main/java/com/demo/provider/InstanceController.java
@@ -0,0 +1,21 @@
+package com.demo.provider;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InstanceController {
+
+    @Value("${server.port}")
+    private String port;
+
+    private final String instaceId = java.util.UUID.randomUUID().toString();
+
+    @GetMapping("/instance-info")
+    public String getInstanceInfo() {
+        System.out.println("In provider running on port: " + port);
+        return "Instance served by port: " + port + " Instance ID: " + instaceId;
+    }
+
+}
