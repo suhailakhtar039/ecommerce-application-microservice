e2cbabdf56fc60588a1b5cb90165af0d15b54a3d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-20 16:51:47 +0530

    define bean of RestClient.Builder

---
+++ consumer/src/main/java/com/demo/consumer/restclient/RestClientConfig.java
@@ -0,0 +1,21 @@
+package com.demo.consumer.restclient;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public RestClient.Builder restClientBuilder(){
+        return RestClient.builder();
+    }
+
+    @Bean
+    public RestClient restClient(RestClient.Builder builder) {
+        return builder.baseUrl("http://localhost:8081")
+                .build();
+    }
+
+}
