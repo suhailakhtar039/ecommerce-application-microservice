b2881449a60504cdd5841fcae62b50a5baaaff9d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-20 23:17:12 +0530

    ProviderWebClient getting used in WebClientController

--- consumer/src/main/java/com/demo/consumer/webclient/WebClientController.java
+++ consumer/src/main/java/com/demo/consumer/webclient/WebClientController.java
@@ -1,5 +1,6 @@
 package com.demo.consumer.webclient;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,16 +9,20 @@ import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/web-client")
+@RequiredArgsConstructor
 public class WebClientController {
+
+    private final ProviderWebClient providerWebClient;
 
     @GetMapping("/instance")
     public Mono<String> getInstance() {
-        WebClient webClient = WebClient.create();
-        Mono<String> response = webClient.get()
-                .uri("http://localhost:8081/instance-info")
-                .retrieve()
-                .bodyToMono(String.class);
-        return response;
+//        WebClient webClient = WebClient.create();
+//        Mono<String> response = webClient.get()
+//                .uri("http://localhost:8081/instance-info")
+//                .retrieve()
+//                .bodyToMono(String.class);
+//        return response;
+        return providerWebClient.getInstanceInfo();
     }
 
 }
