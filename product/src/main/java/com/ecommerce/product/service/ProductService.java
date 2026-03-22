ca576b2cb3373cc54c57f584be4fbeffe1f50df0
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-22 20:24:32 +0530

    getProductById is implemented

--- product/src/main/java/com/ecommerce/product/service/ProductService.java
+++ product/src/main/java/com/ecommerce/product/service/ProductService.java
@@ -79,4 +79,9 @@ public class ProductService {
                 .map(this::mapToProductResponse)
                 .collect(Collectors.toList());
     }
+
+    public Optional<ProductResponse> getProductById(String id) {
+        return productRepository.findByIdAndActiveTrue(Long.valueOf(id))
+                .map(this::mapToProductResponse);
+    }
 }
