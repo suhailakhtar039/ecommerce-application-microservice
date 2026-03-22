20713abdb4a9da5e1040db6e4d0e3f3c2184f3ec
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-22 20:24:07 +0530

    find by Id and Active true

--- product/src/main/java/com/ecommerce/product/repository/ProductRepository.java
+++ product/src/main/java/com/ecommerce/product/repository/ProductRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.nio.channels.FileChannel;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
@@ -15,4 +17,6 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
             " AND p.stockQuantity > 0" +
             " AND LOWER(p.name) LIKE LOWER(CONCAT('%',:keyword,'%'))")
     List<Product> searchProducts(String keyword);
+
+    Optional<Product> findByIdAndActiveTrue(Long id);
 }
