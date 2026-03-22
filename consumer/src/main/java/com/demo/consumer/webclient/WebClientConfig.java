2357d1f39618fbdbc58897dded2548549c0c3ffe
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-22 12:05:46 +0530

    use @LoadBalanced in bean of builder and change URL in second one

--- consumer/src/main/java/com/demo/consumer/webclient/WebClientConfig.java
+++ consumer/src/main/java/com/demo/consumer/webclient/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.demo.consumer.webclient;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -8,13 +9,14 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class WebClientConfig {
 
     @Bean
+    @LoadBalanced
     public WebClient.Builder webClientBuilder(){
         return WebClient.builder();
     }
 
     @Bean
     public WebClient webClient(WebClient.Builder builder){
-        return builder.baseUrl("http://localhost:8081")
+        return builder.baseUrl("http://provider")
                 .build();
     }
 }
