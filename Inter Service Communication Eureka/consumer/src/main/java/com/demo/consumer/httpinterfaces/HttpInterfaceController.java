422f55027fbd9e2c3f9235ec82e495dcfa0df6be
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-03-23 22:24:27 +0530

    controller created for HttpInterfaceController

---
+++ Inter Service Communication Eureka/consumer/src/main/java/com/demo/consumer/httpinterfaces/HttpInterfaceController.java
@@ -0,0 +1,18 @@
+package com.demo.consumer.httpinterfaces;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/http-interface")
+@RequiredArgsConstructor
+public class HttpInterfaceController {
+    private final ProviderHttpInterface providerHttpInterface;
+
+    @GetMapping("/instance")
+    public String getInstanceDetails(){
+        return providerHttpInterface.getInstanceInfo();
+    }
+}
