9726448bbeb029663fa5ad6cf825ed89770574e0
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-22 21:01:11 +0530

    JwtAuthFilter created for testing purpose of header

---
+++ gateway/src/main/java/com/ecommerce/gateway/JwtAuthFilter.java
@@ -0,0 +1,39 @@
+package com.ecommerce.gateway;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthFilter implements WebFilter {
+    /**
+     * Process the Web request and (optionally) delegate to the next
+     * {@code WebFilter} through the given {@link WebFilterChain}.
+     *
+     * @param exchange the current server exchange
+     * @param chain    provides a way to delegate to the next filter
+     * @return {@code Mono<Void>} to indicate when request processing is complete
+     */
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        String authHeader = exchange
+                .getRequest()
+                .getHeaders()
+                .getFirst(HttpHeaders.AUTHORIZATION);
+
+        if(authHeader == null || !authHeader.startsWith("Bearer")){
+                    exchange
+                    .getResponse()
+                    .setStatusCode(HttpStatus.UNAUTHORIZED);
+
+            return exchange
+                    .getResponse()
+                    .setComplete();
+        }
+        return chain.filter(exchange);
+    }
+}
