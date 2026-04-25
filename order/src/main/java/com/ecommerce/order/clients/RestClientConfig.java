9db53431aa1da2078e29f1df6551f3e53a54df9d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-25 15:43:01 +0530

    created two simple beans and mark one of them primary

--- order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
@@ -4,17 +4,15 @@ import io.micrometer.observation.ObservationRegistry;
 import io.micrometer.tracing.Tracer;
 import io.micrometer.tracing.propagation.Propagator;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
-import org.springframework.web.client.support.RestClientAdapter;
-import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 @Configuration
-public class ProductServiceClientConfig {
+public class RestClientConfig {
     @Autowired(required = false)
     private ObservationRegistry observationRegistry;
 
@@ -25,34 +23,22 @@ public class ProductServiceClientConfig {
     private Propagator propagator;
 
     @Bean
-    @LoadBalanced
-    public RestClient.Builder loadBalancedRestClientBuilder() {
+    @Primary
+    public RestClient.Builder restClientBuilder() {
         return RestClient.builder();
     }
 
     private ClientHttpRequestInterceptor createTracingInterceptor() {
         return ((request, body, execution) -> {
-            if (tracer != null && propagator != null && tracer.currentSpan() != null) {
+            if (tracer != null && propagator != null
+                    && tracer.currentSpan() != null) {
                 propagator.inject(tracer.currentTraceContext().context(),
                         request.getHeaders(),
                         (carrier, key, value) -> carrier.add(key, value));
             }
             return execution.execute(request, body);
-        });
+        }
+        );
     }
 
-    @Bean
-    public ProductServiceClient restClientInterface(@Qualifier("loadBalancedRestClientBuilder")
-                                                        RestClient.Builder restClientBuilder) {
-        RestClient restClient = restClientBuilder
-                .baseUrl("http://product-service")
-                .build();
-
-        RestClientAdapter adapter = RestClientAdapter.create(restClient);
-        HttpServiceProxyFactory factory = HttpServiceProxyFactory
-                .builderFor(adapter)
-                .build();
-        return factory.createClient(ProductServiceClient.class);
-
-    }
 }
