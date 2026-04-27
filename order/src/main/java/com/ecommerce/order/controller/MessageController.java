fd8336f7d6f8b82008b47f951a1fcf0a061075e7
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-27 19:35:12 +0530

    using rate limiter

--- order/src/main/java/com/ecommerce/order/controller/MessageController.java
+++ order/src/main/java/com/ecommerce/order/controller/MessageController.java
@@ -1,5 +1,6 @@
 package com.ecommerce.order.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,10 @@ public class MessageController {
     private String message;
 
     @GetMapping("/message")
+    @Retry(name = "retryBreaker", fallbackMethod = "fallbackRetry")
     public String getMessage(){
         return message;
     }
+
+
 }
