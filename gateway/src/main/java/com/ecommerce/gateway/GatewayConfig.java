9c8a01c84a629ebed558063b92622e9b1de77036
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-28 19:47:20 +0530

    redis cached inserted into pom.xml and setup in GatewayConfig

--- gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
+++ gateway/src/main/java/com/ecommerce/gateway/GatewayConfig.java
@@ -1,13 +1,28 @@
 package com.ecommerce.gateway;
 
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(1, 1, 1);
+    }
+
+    @Bean
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest().getRemoteAddress().getHostName()
+        );
+    }
 
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
@@ -19,7 +34,10 @@ public class GatewayConfig {
                                                 .setRetries(10)
                                                 .setMethods(HttpMethod.GET)
                                         )
-
+                                        .requestRateLimiter(config -> config
+                                                .setRateLimiter(redisRateLimiter())
+                                                .setKeyResolver(hostNameKeyResolver())
+                                        )
                                         .circuitBreaker(c ->
                                                 c.setName("ecomBreaker")
                                                         .setFallbackUri("forward:/fallback/products")))
