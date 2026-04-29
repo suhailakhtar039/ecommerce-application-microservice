8973c1575ec47073b7f9c6de9903e18477252e2d
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-29 19:21:30 +0530

    RabbitMQConfiguration for notification service

---
+++ notification/src/main/java/com/ecommerce/notification/RabbitMQConfiguration.java
@@ -0,0 +1,59 @@
+package com.ecommerce.notification;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.ExchangeBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue queue() {
+        return QueueBuilder
+                .durable(queueName)
+                .build();
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return ExchangeBuilder
+                .topicExchange(exchangeName)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+}
