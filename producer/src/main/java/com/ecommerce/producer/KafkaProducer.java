dd7de6064a92823320ee5b4e11b95561aa03fcfa
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-29 22:55:23 +0530

    add configuration and simple controller

---
+++ producer/src/main/java/com/ecommerce/producer/KafkaProducer.java
@@ -0,0 +1,25 @@
+package com.ecommerce.producer;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class KafkaProducer {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaTemplate.send("my-topic", message);
+        return "Message sent: " + message;
+    }
+
+}
