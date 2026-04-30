558cd8ce8f2fce5739f3867b7032c3208ef0cdab
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-04-30 20:12:55 +0530

    using kafka listener in KafkaConsumer

---
+++ consumer/src/main/java/com/ecommerce/consumer/KafkaConsumer.java
@@ -0,0 +1,14 @@
+package com.ecommerce.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+    public void listen(String message){
+        System.out.println("Received Message: " + message);
+    }
+
+}
