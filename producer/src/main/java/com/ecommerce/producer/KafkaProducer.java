d33291938b1944879e7ede0e3222c689b0652f5a
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-01 20:42:06 +0530

    kafka template changed to send RiderGroup

--- producer/src/main/java/com/ecommerce/producer/KafkaProducer.java
+++ producer/src/main/java/com/ecommerce/producer/KafkaProducer.java
@@ -10,16 +10,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class KafkaProducer {
 
-    private final KafkaTemplate<String, String> kafkaTemplate;
+    private final KafkaTemplate<String, RiderLocation> kafkaTemplate;
 
-    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+    public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
         this.kafkaTemplate = kafkaTemplate;
     }
 
     @PostMapping("/send")
     public String sendMessage(@RequestParam String message) {
-        kafkaTemplate.send("my-topic", message);
-        return "Message sent: " + message;
+        RiderLocation riderLocation =
+                new RiderLocation("rider1", 39.12, 98.23);
+        kafkaTemplate.send("my-topic", riderLocation);
+        return "Message sent: " + riderLocation.getRiderId();
     }
 
 }
