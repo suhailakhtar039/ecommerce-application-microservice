b04c7368b13b76b954fbdecc52068fc83e8dd7ec
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-02 13:17:17 +0530

    commented out

--- producer/src/main/java/com/ecommerce/producer/KafkaProducer.java
+++ producer/src/main/java/com/ecommerce/producer/KafkaProducer.java
@@ -1,27 +1,27 @@
 package com.ecommerce.producer;
-
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api")
-public class KafkaProducer {
-
-    private final KafkaTemplate<String, RiderLocation> kafkaTemplate;
-
-    public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    @PostMapping("/send")
-    public String sendMessage(@RequestParam String message) {
-        RiderLocation riderLocation =
-                new RiderLocation("rider1", 39.12, 98.23);
-        kafkaTemplate.send("my-topic", riderLocation);
-        return "Message sent: " + riderLocation.getRiderId();
-    }
-
-}
+//
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api")
+//public class KafkaProducer {
+//
+//    private final KafkaTemplate<String, RiderLocation> kafkaTemplate;
+//
+//    public KafkaProducer(KafkaTemplate<String, RiderLocation> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    @PostMapping("/send")
+//    public String sendMessage(@RequestParam String message) {
+//        RiderLocation riderLocation =
+//                new RiderLocation("rider1", 39.12, 98.23);
+//        kafkaTemplate.send("my-topic", riderLocation);
+//        return "Message sent: " + riderLocation.getRiderId();
+//    }
+//
+//}
