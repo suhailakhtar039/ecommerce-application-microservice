b0e4548472ffd630ff41d0180a179d6a5b56d1f0
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-02 13:45:58 +0530

    generating random rider Id

--- producer/src/main/java/com/ecommerce/producer/KafkaProducerStreams.java
+++ producer/src/main/java/com/ecommerce/producer/KafkaProducerStreams.java
@@ -3,15 +3,18 @@ package com.ecommerce.producer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Random;
 import java.util.function.Supplier;
 
 @Configuration
 public class KafkaProducerStreams {
     @Bean
     public Supplier<RiderLocation> sendRiderLocation(){
+        Random random = new Random();
+        String riderId = "riderId " + random.nextInt(100);
         return () -> {
-            RiderLocation location = new RiderLocation("ryder1", 11.0, 12.3);
-            System.out.println("Sending From sendRiderLocation method: " + location.getRiderId());
+            RiderLocation location = new RiderLocation(riderId, 11.0, 12.3);
+            System.out.println("Sending From sendRiderLocation method: " + riderId);
             return location;
         };
     }
