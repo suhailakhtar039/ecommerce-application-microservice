5774408c089583a4e6e35d80da65271c83817602
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-02 14:02:58 +0530

    riderStatus created

--- producer/src/main/java/com/ecommerce/producer/KafkaProducerStreams.java
+++ producer/src/main/java/com/ecommerce/producer/KafkaProducerStreams.java
@@ -2,6 +2,11 @@ package com.ecommerce.producer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.util.MimeTypeUtils;
 
 import java.util.Random;
 import java.util.function.Supplier;
@@ -16,6 +21,20 @@ public class KafkaProducerStreams {
             RiderLocation location = new RiderLocation(riderId, 11.0, 12.3);
             System.out.println("Sending From sendRiderLocation method: " + riderId);
             return location;
+        };
+    }
+
+    @Bean
+    public Supplier<Message<String>> sendRiderStatus(){
+        Random random = new Random();
+        return () -> {
+            String riderId = "riderId " + random.nextInt(100);
+            String status = random.nextBoolean() ? "Ride started" : "Ride ended";
+            System.out.println("Sending : " + status);
+            return MessageBuilder.withPayload(riderId + " : " + status)
+                    .setHeader(KafkaHeaders.KEY, riderId.getBytes())
+                    .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.TEXT_PLAIN)
+                    .build();
         };
     }
 }
