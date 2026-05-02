9924899365f87902dcf18d28c4d23ac9d371f618
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-02 13:44:49 +0530

    commented out KafkaConsumer.java

--- consumer/src/main/java/com/ecommerce/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/ecommerce/consumer/KafkaConsumer.java
@@ -1,26 +1,26 @@
 package com.ecommerce.consumer;
-
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class KafkaConsumer {
-
-    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
-    public void listen1(String message){
-        System.out.println("Received Message1 : " + message);
-    }
-
-    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
-    public void listen2(String message){
-        System.out.println("Received Message2 : " + message);
-    }
-
-    @KafkaListener(topics = "my-topic-new", groupId = "my-new-group-rider")
-    public void listenRiderLocation(RiderLocation location){
-        System.out.println("Received location : " +
-                location.getRiderId() + " : " +
-                location.getLatitude() + " : " + location.getLongitude());
-    }
-
-}
+//
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class KafkaConsumer {
+//
+//    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+//    public void listen1(String message){
+//        System.out.println("Received Message1 : " + message);
+//    }
+//
+//    @KafkaListener(topics = "my-topic", groupId = "my-new-group")
+//    public void listen2(String message){
+//        System.out.println("Received Message2 : " + message);
+//    }
+//
+//    @KafkaListener(topics = "my-topic-new", groupId = "my-new-group-rider")
+//    public void listenRiderLocation(RiderLocation location){
+//        System.out.println("Received location : " +
+//                location.getRiderId() + " : " +
+//                location.getLatitude() + " : " + location.getLongitude());
+//    }
+//
+//}
