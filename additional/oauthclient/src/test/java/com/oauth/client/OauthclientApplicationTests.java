6b366a34e791fd377729ddbf574718f7e451138e
Suhail Akhtar <suhailakhtar039@gmail.com>
2026-05-06 19:52:43 +0530

    oauthclient and resource created

---
+++ additional/oauthclient/src/test/java/com/oauth/client/OauthclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
